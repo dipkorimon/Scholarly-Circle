79b2f7e6673e1e032c031fab9508be83fb4027ea
dipkorimon <dipkorimon@gamil.com>
2023-09-03 07:15:19 +0600

    updated

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import "./footer.scss";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="footer">
+        <div className="top">
+          <a href="https://www.linkedin.com/in/dipkorimon/">
+            <LinkedInIcon />
+          </a>
+          <a href="https://www.facebook.com/dipkorimon/">
+            <FacebookIcon />
+          </a>
+          <a href="https://github.com/dipkorimon">
+            <GitHubIcon />
+          </a>
+          <a href="/contact">
+            <EmailIcon />
+          </a>
+        </div>
+        <div className="mid">
+          <p>
+            Copyright &copy; <span>Scholarly Circle</span>
+          </p>
+        </div>
+        <div className="bottom">
+          <p>
+            Made with ❤️ by <span>Dip Kor Imon</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./navBar.scss";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <div className="navBar">
+        <div className="left">
+          <a href="/">
+            <img src="./../../public/logo.png" alt="" />
+          </a>
+          <a href="/">ScholarlyCircle</a>
+        </div>
+        <div className="middle">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+          <a href="/developerInfo">Developer Info</a>
+        </div>
+        <div className="right">
+          <a href="/register" className="regi">
+            Sign up
+          </a>
+          <a href="/login" className="log">
+            Sign in
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
