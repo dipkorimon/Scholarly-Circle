697c04f3d23f3cd0e22374f4ee4f29bf740cb843
dipkorimon <dipkorimon@gamil.com>
2023-12-15 02:01:31 +0600

    created accounts for authors

--- client/src/pages/addAuthor/AddAuthor.jsx
+++ client/src/pages/addAuthor/AddAuthor.jsx
@@ -41,8 +41,8 @@ const AddAuthor = () => {
   return (
     <div className="addAuthor">
       <h1>
-        Add author with student ID, full name, email, session, batch, phone,
-        blood group, defense date and a photo.
+        Add author with student ID, full name, email, password, session, batch,
+        phone, blood group, defense date and a photo.
       </h1>
       <p>
         An asterisk (<span>*</span>) indicates a required field
@@ -83,19 +83,48 @@ const AddAuthor = () => {
         <label htmlFor="">
           Session<span>*</span>
         </label>
-        <input
-          type="text"
-          required
-          onChange={(e) => setSession(e.target.value)}
-        />
+        <select name="" id="" onChange={(e) => setSession(e.target.value)}>
+          <option value="2006-2007">2006-2007</option>
+          <option value="2007-2008">2007-2008</option>
+          <option value="2008-2009">2008-2009</option>
+          <option value="2009-2010">2009-2010</option>
+          <option value="2010-2011">2010-2011</option>
+          <option value="2011-2012">2011-2012</option>
+          <option value="2012-2013">2012-2013</option>
+          <option value="2013-2014">2013-2014</option>
+          <option value="2014-2015">2014-2015</option>
+          <option value="2015-2016">2015-2016</option>
+          <option value="2016-2017">2016-2017</option>
+          <option value="2017-2018">2017-2018</option>
+          <option value="2018-2019">2018-2019</option>
+          <option value="2019-2020">2019-2020</option>
+          <option value="2020-2021">2020-2021</option>
+          <option value="2021-2022">2021-2022</option>
+          <option value="2022-2023">2022-2023</option>
+        </select>
         <label htmlFor="">
           Batch<span>*</span>
         </label>
-        <input
-          type="text"
-          required
-          onChange={(e) => setBatch(e.target.value)}
-        />
+        <select name="" id="" onChange={(e) => setBatch(e.target.value)}>
+          <option value="01">01</option>
+          <option value="02">02</option>
+          <option value="03">03</option>
+          <option value="04">04</option>
+          <option value="05">05</option>
+          <option value="06">06</option>
+          <option value="07">07</option>
+          <option value="08">08</option>
+          <option value="09">09</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="18">18</option>
+        </select>
         <label htmlFor="">
           Phone<span>*</span>
         </label>
